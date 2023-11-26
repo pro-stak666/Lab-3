0c8a3976577282ca09e9fd129134d7e1735ac6c1
pro-stak666 <mineev_kirill2005@mail.ru>
2023-11-26 22:03:00 +0300

    res

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import tkinter as tk
 from tkinter import messagebox
-from sqrEq import sqrEquation
+import random
+
+ALPHABET = list('QWERTYUIOPASDFGHJKLZXCVBNM')
+
+
+def check(n):
+    if len(n) == 6 and all([i.isdigit() for i in n]):
+        return True
+    return False
+
+
+def generate_key():
+    n = arg_A.get()
+    if check(n):
+        first_block = list(n[3:]) + random.choices(ALPHABET, k=2)
+        second_block = list(n[:3]) + random.choices(ALPHABET, k=2)
+        random.shuffle(first_block)
+        random.shuffle(second_block)
+        third_block = str(int(''.join((filter(lambda x: x.isdigit(), first_block)))) + int(''.join((filter(lambda x: x.isdigit(), second_block))))).rjust(4, '0')
+        res_arg.configure(text=f"{''.join(first_block)}-{''.join(second_block)} {third_block}")
+    else:
+        tk.messagebox.showwarning('Ошибка', 'Число не удовлетворяет условиям!')
 
 
 def close():
     window.destroy()
 
 
-def calc():
-    A = float(arg_A.get())
-    B = float(arg_B.get())
-    C = float(arg_C.get())
-    if A == 0.0:
-        tk.messagebox.showwarning('Error', 'Division by zero!')
-    else:
-        lbl_result.configure(text=sqrEquation(A, B, C))
-
-
 window = tk.Tk()
-window.geometry('576x360')
-bg_img = tk.PhotoImage(file='bg_pic.png')
+window.geometry('1000x800')
+bg_img = tk.PhotoImage(file='cm.png')
 
 lbl_bg = tk.Label(window, image=bg_img)
 lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
@@ -27,30 +38,15 @@ lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
 frame = tk.Frame(window)
 frame.place(relx=0.5, rely=0.5, anchor='center')
 
-lbl_A = tk.Label(frame, text='A', font=('Arial', 30), bg='blue', fg='white')
-lbl_A.grid(column=0, row=0, padx=10, pady=15)
+lbl_A = tk.Label(frame, text='Введите 6-ти значное число', font=('Arial', 30))
+lbl_A.grid(column=1, row=1, padx=10, pady=15)
 arg_A = tk.Entry(frame, width=10)
-arg_A.insert(0, '1')
-arg_A.grid(column=0, row=1, padx=10, pady=15)
+arg_A.grid(column=1, row=2, padx=10, pady=15)
 
-lbl_B = tk.Label(frame, text='B', font=('Arial', 30))
-lbl_B.grid(column=1, row=0, padx=10, pady=15)
-arg_B = tk.Entry(frame, width=10)
-arg_B.insert(0, '0')
-arg_B.grid(column=1, row=1, padx=10, pady=15)
+res_arg = tk.Label(frame, width=20)
+res_arg.grid(column=1, row=3)
 
-lbl_C = tk.Label(frame, text='C', font=('Arial', 30))
-lbl_C.grid(column=2, row=0, padx=10, pady=15)
-arg_C = tk.Entry(frame, width=10)
-arg_C.insert(0, '0')
-arg_C.grid(column=2, row=1, padx=10, pady=15)
-
-lbl_roots = tk.Label(frame, text='Result:')
-lbl_roots.grid(column=1, row=2)
-lbl_result = tk.Label(frame, text='None yet.', font=('Arial', 10))
-lbl_result.grid(column=2, row=2)
-
-btn_calc = tk.Button(frame, text='Calculate', command=calc)
+btn_calc = tk.Button(frame, text='сгенерировать ключ', command=generate_key)
 btn_calc.grid(column=0, row=3, padx=10, pady=15)
 btn_exit = tk.Button(frame, text='Cancel', command=close)
 btn_exit.grid(column=2, row=3, padx=10, pady=15)
